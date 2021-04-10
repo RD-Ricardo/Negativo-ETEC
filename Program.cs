3236ed6568597801fa2e88ce52569ae3f5220632
RD <ricardodias169@gmail.com>
2021-04-10 18:50:18 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double  num1;
+            Console.WriteLine("------------");
+            Console.WriteLine("--Negativo--");
+            Console.WriteLine("------------");
+            Console.Write("Digite um número: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            if(num1 <= 0 ){
+                Console.WriteLine($"\nVocê digitou um número negativo {num1}");
+            }
+        }
+    }
+}
